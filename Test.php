4012216ac5428fca093ce650aa9ddf52ed7d9f12
Steffen Pott <pott@wabsolute.de>
2016-03-19 14:47:04 +0100

    phpunit test first try

---
+++ Test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * User: Steffen
+ * Date: 19.03.2016
+ */
+class Test extends PHPUnit_Framework_TestCase
+{
+    public function testOnePlusOne() {
+        $this->assertEquals(1+1,1);
+    }
+}
